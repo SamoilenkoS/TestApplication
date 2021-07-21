0338a212bba36e10459f1d92034ee57440fe7115
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-07-22 01:32:39 +0300

    Step4: added attribute validation

--- DataAccessLayer/Models/AuthenticationModel.cs
+++ DataAccessLayer/Models/AuthenticationModel.cs
@@ -1,8 +1,17 @@
-﻿namespace DataAccessLayer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models
 {
     public class AuthenticationModel
     {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
         public  string Login { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
         public  string Password { get; set; }
     }
 }
--- WebApplication.Database/Procedures/GetAllUserRoles.sql
+++ WebApplication.Database/Procedures/GetAllUserRoles.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllUserRoles]
 AS
     SELECT u.Id UserId,
-    r.ROLE [Role]
+    r.[Role]
 FROM Users u
 INNER JOIN UserRoles ur ON ur.UserId = u.Id
 INNER JOIN Roles r ON ur.RoleId = r.Id
